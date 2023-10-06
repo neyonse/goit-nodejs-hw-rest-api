b5a22f56c4d4935e77c29a24f9a6aeaf57e9faac
Anastasiia Vedmid <neyonse@gmail.com>
2023-10-06 20:00:44 +0300

    connected MongoDB and configured work with it

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,15 +1,15 @@
-import contactsServices from "../models/contacts.js";
 import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
+import Contact from "../models/Contact.js";
 
 const getAll = async (req, res, next) => {
-  const result = await contactsServices.getAllContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getById = async (req, res, next) => {
   const { id } = req.params;
-  const result = await contactsServices.getContactById(id);
+  const result = await Contact.findById(id);
 
   if (!result) {
     throw HttpError(404);
@@ -19,13 +19,38 @@ const getById = async (req, res, next) => {
 };
 
 const add = async (req, res, next) => {
-  const result = await contactsServices.addContact(req.body);
+  const result = await Contact.create(req.body);
+
+  res.status(201).json(result);
+};
+
+const updateById = async (req, res, next) => {
+  const { id } = req.params;
+
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+
+  if (!result) {
+    throw HttpError(404);
+  }
+
+  res.status(201).json(result);
+};
+
+const updateStatusContact = async (req, res, next) => {
+  const { id } = req.params;
+
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+
+  if (!result) {
+    throw HttpError(404);
+  }
+
   res.status(201).json(result);
 };
 
 const removeById = async (req, res, next) => {
   const { id } = req.params;
-  const result = await contactsServices.removeContact(id);
+  const result = await Contact.findByIdAndRemove(id);
 
   if (!result) {
     throw HttpError(404);
@@ -34,22 +59,11 @@ const removeById = async (req, res, next) => {
   res.json({ message: "contact deleted" });
 };
 
-const updateById = async (req, res, next) => {
-  const { id } = req.params;
-
-  const result = await contactsServices.updateContact(id, req.body);
-
-  if (!result) {
-    throw HttpError(404);
-  }
-
-  res.status(201).json(result);
-};
-
 export default {
   getAll: ctrlWrapper(getAll),
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
-  removeById: ctrlWrapper(removeById),
   updateById: ctrlWrapper(updateById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  removeById: ctrlWrapper(removeById),
 };
